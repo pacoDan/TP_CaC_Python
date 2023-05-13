8945cf0a2267df0789cd3a2f4b99315401ada224
pacodan <jhonpaco@frba.utn.edu.ar>
2023-05-13 16:48:15 -0300

    :fire: en js separado

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        title: 'Ingresar a pedidos',
-        username: '',
-        password: '',
-        loginSuccess: false, // Estado de inicio de sesión exitoso
-        loginError: false // Estado de inicio de sesión fallido
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Ingresar a pedidos',
+            username: '',
+            password: '',
+            loginSuccess: false,
+            loginError: false
+        };
     },
     created() {
-        // Realizar la llamada GET para obtener la lista de usuarios
         fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
             .then(response => response.json())
             .then(data => {
@@ -22,17 +22,13 @@ const app = new Vue({
         login() {
             const user = this.users.find(user => user.username === this.username);
             if (user && user.password === this.password) {
-                // Usuario autenticado correctamente
                 this.loginSuccess = true;
                 this.loginError = false;
             } else {
-                // Credenciales inválidas
                 this.loginSuccess = false;
                 this.loginError = true;
             }
         }
     }
 });
-
-
-// module.exports = app;
+app.mount('#app');
