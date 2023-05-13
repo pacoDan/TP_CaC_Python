b0b4bc164a6ef77ddc8cb2f7c1ea1927371c1aa8
pacodan <jhonpaco@frba.utn.edu.ar>
2023-05-13 16:46:19 -0300

    :fire: createApp de vue ok

--- js/login.js
+++ js/login.js
@@ -1,28 +1,26 @@
-var datos_login="https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json"
-new Vue({
-    el: "#app",
+const app = new Vue({
+    el: '#app',
     data: {
-        title: "Ingresar a pedidos",
-        username: "",
-        password: "",
+        title: 'Ingresar a pedidos',
+        username: '',
+        password: '',
         loginSuccess: false, // Estado de inicio de sesión exitoso
-        loginError: false, // Estado de inicio de sesión fallido
-        users: [] // Lista de usuarios obtenida del archivo JSON
+        loginError: false // Estado de inicio de sesión fallido
     },
     created() {
         // Realizar la llamada GET para obtener la lista de usuarios
-        fetch(datos_login)
-            .then((response) => response.json())
-            .then((data) => {
+        fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
+            .then(response => response.json())
+            .then(data => {
                 this.users = data;
             })
-            .catch((error) => {
-                console.error("Error al obtener la lista de usuarios", error);
+            .catch(error => {
+                console.error('Error al obtener la lista de usuarios', error);
             });
     },
     methods: {
         login() {
-            const user = this.users.find((user) => user.username === this.username);
+            const user = this.users.find(user => user.username === this.username);
             if (user && user.password === this.password) {
                 // Usuario autenticado correctamente
                 this.loginSuccess = true;
@@ -32,44 +30,9 @@ new Vue({
                 this.loginSuccess = false;
                 this.loginError = true;
             }
-        },
-    },
+        }
+    }
 });
 
 
-
-// new Vue({
-//     el: '#app',
-//     data: {
-//       title: 'Ingresar a pedidos',
-//       username: '',
-//       password: '',
-//       loginSuccess: false, // Estado de inicio de sesión exitoso
-//       loginError: false // Estado de inicio de sesión fallido
-//     },
-//     created() {
-//       // Realizar la llamada GET para obtener la lista de usuarios
-//       fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
-//         .then(response => response.json())
-//         .then(data => {
-//           this.users = data;
-//         })
-//         .catch(error => {
-//           console.error('Error al obtener la lista de usuarios', error);
-//         });
-//     },
-//     methods: {
-//       login() {
-//         const user = this.users.find(user => user.username === this.username);
-//         if (user && user.password === this.password) {
-//           // Usuario autenticado correctamente
-//           this.loginSuccess = true;
-//           this.loginError = false;
-//         } else {
-//           // Credenciales inválidas
-//           this.loginSuccess = false;
-//           this.loginError = true;
-//         }
-//       }
-//     }
-//   });
+// module.exports = app;
