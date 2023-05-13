447de878c39559cd21041cec77b1d685475c479b
pacodan <jhonpaco@frba.utn.edu.ar>
2023-05-13 13:02:02 -0300

    con datos en login

---
+++ js/login.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '#app',
+    data: {
+      title: 'Ingresar a pedidos',
+      username: '',
+      password: '',
+      loginSuccess: false, // Estado de inicio de sesión exitoso
+      loginError: false // Estado de inicio de sesión fallido
+    },
+    created() {
+      // Realizar la llamada GET para obtener la lista de usuarios
+      fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
+        .then(response => response.json())
+        .then(data => {
+          this.users = data;
+        })
+        .catch(error => {
+          console.error('Error al obtener la lista de usuarios', error);
+        });
+    },
+    methods: {
+      login() {
+        const user = this.users.find(user => user.username === this.username);
+        if (user && user.password === this.password) {
+          // Usuario autenticado correctamente
+          this.loginSuccess = true;
+          this.loginError = false;
+        } else {
+          // Credenciales inválidas
+          this.loginSuccess = false;
+          this.loginError = true;
+        }
+      }
+    }
+  });
