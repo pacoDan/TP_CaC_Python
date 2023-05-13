db577e518f340eabbb3624418009d3b077eecc84
pacodan <jhonpaco@frba.utn.edu.ar>
2023-05-13 15:18:37 -0300

    refactor usuario.html

--- js/login.js
+++ js/login.js
@@ -1,35 +1,74 @@
 new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-      title: 'Ingresar a pedidos',
-      username: '',
-      password: '',
-      loginSuccess: false, // Estado de inicio de sesión exitoso
-      loginError: false // Estado de inicio de sesión fallido
+        title: "Ingresar a pedidos",
+        username: "",
+        password: "",
+        loginSuccess: false, // Estado de inicio de sesión exitoso
+        loginError: false, // Estado de inicio de sesión fallido
+        users: [] // Lista de usuarios obtenida del archivo JSON
     },
     created() {
-      // Realizar la llamada GET para obtener la lista de usuarios
-      fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
-        .then(response => response.json())
-        .then(data => {
-          this.users = data;
-        })
-        .catch(error => {
-          console.error('Error al obtener la lista de usuarios', error);
-        });
+        // Realizar la llamada GET para obtener la lista de usuarios
+        fetch("https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json")
+            .then((response) => response.json())
+            .then((data) => {
+                this.users = data;
+            })
+            .catch((error) => {
+                console.error("Error al obtener la lista de usuarios", error);
+            });
     },
     methods: {
-      login() {
-        const user = this.users.find(user => user.username === this.username);
-        if (user && user.password === this.password) {
-          // Usuario autenticado correctamente
-          this.loginSuccess = true;
-          this.loginError = false;
-        } else {
-          // Credenciales inválidas
-          this.loginSuccess = false;
-          this.loginError = true;
-        }
-      }
-    }
-  });
+        login() {
+            const user = this.users.find((user) => user.username === this.username);
+            if (user && user.password === this.password) {
+                // Usuario autenticado correctamente
+                this.loginSuccess = true;
+                this.loginError = false;
+            } else {
+                // Credenciales inválidas
+                this.loginSuccess = false;
+                this.loginError = true;
+            }
+        },
+    },
+});
+
+
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//       title: 'Ingresar a pedidos',
+//       username: '',
+//       password: '',
+//       loginSuccess: false, // Estado de inicio de sesión exitoso
+//       loginError: false // Estado de inicio de sesión fallido
+//     },
+//     created() {
+//       // Realizar la llamada GET para obtener la lista de usuarios
+//       fetch('https://raw.githubusercontent.com/pacodan/restaurant/main/datos/login.json')
+//         .then(response => response.json())
+//         .then(data => {
+//           this.users = data;
+//         })
+//         .catch(error => {
+//           console.error('Error al obtener la lista de usuarios', error);
+//         });
+//     },
+//     methods: {
+//       login() {
+//         const user = this.users.find(user => user.username === this.username);
+//         if (user && user.password === this.password) {
+//           // Usuario autenticado correctamente
+//           this.loginSuccess = true;
+//           this.loginError = false;
+//         } else {
+//           // Credenciales inválidas
+//           this.loginSuccess = false;
+//           this.loginError = true;
+//         }
+//       }
+//     }
+//   });
