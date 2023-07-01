d2d740654e2c4a8e1fed756773786e15b6fa87ed
pacoDan <jhonpaco@frba.utn.edu.ar>
2023-06-30 23:30:27 -0300

    :fire: app.py de productos de ejemplo

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
-class Producto:
-    def __init__(self, codigo, nombre, descripcion, cantidad, precio):
-        self.codigo = codigo
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.cantidad = cantidad
-        self.precio = precio
+from modelo.Producto import Producto  # Importa la clase Producto del archivo Producto.py
 
 app = Flask(__name__)
 CORS(app)
 
+# Esto es solo un ejemplo, normalmente estos datos vendrían de una base de datos.
 productos = [
-    Producto('001', 'Producto1', 'Descripción1', 10, 100.0),
-    Producto('002', 'Producto2', 'Descripción2', 20, 200.0),
-    Producto('003', 'Producto3', 'Descripción3', 30, 300.0),
+    Producto('001', 'Descripción1', 10, 100.0),
+    Producto('002', 'Descripción2', 20, 200.0),
+    Producto('003', 'Descripción3', 30, 300.0),
 ]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Bienvenido a la API de Productos!"
 
 @app.route('/productos', methods=['GET'])
 def get_productos():
